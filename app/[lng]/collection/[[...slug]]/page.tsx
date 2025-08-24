2f2747ac55b776bf93bf9c0eb9405316e1e45f35
Avshi <avshalomsh@mta.ac.il>
2025-08-24 15:00:37 +0300

    AAdding a little more sizes to the filter navigation

--- app/[lng]/collection/[[...slug]]/page.tsx
+++ app/[lng]/collection/[[...slug]]/page.tsx
@@ -154,7 +154,9 @@ export default function CollectionSlugPage() {
   const allSizes = [
     ...new Set([
       ...products.flatMap((p) => p.variants?.map((v) => v.size).filter(Boolean) || []),
-      ...products.flatMap((p) => p.sizes || [])
+      ...products.flatMap((p) => p.sizes || []),
+      // Add any additional sizes you want to always show
+      "35", "35.5", "36", "36.5", "37", "37.5", "38", "38.5", "39", "39.5", "40", "40.5", "41", "41.5", "42", "42.5", "43", "43.5", "44", "44.5", "45"
     ]),
   ] as string[];
 

1844a8f8566b92aca367dc147bdf6f8b93edf323
Avshi <avshalomsh@mta.ac.il>
2025-10-03 10:08:25 +0300

    Update TypeScript references and package dependencies; add email script command

---
+++ app/emails/welcome.tsx
@@ -0,0 +1,18 @@
+import { Button, Html, Body } from "@react-email/components";
+import * as React from "react";
+import 'web-streams-polyfill/polyfill';
+
+export default function Welcome() {
+  return (
+    <Html>
+      <Body>
+        <Button
+          href="https://example.com"
+          style={{ background: "#000", color: "#fff", padding: "12px 20px" }}
+        >
+          Click me
+        </Button>
+      </Body>
+    </Html>
+  );
+}

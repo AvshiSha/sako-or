973e298f517534220b419d32641cef99a6fdcb0a
Avshi <avshalomsh@mta.ac.il>
2025-11-06 07:58:21 +0200

    refactor: remove legacy Google Analytics tracking scripts
    - Eliminated the old gtag.js tracking scripts from the layout file to streamline the integration of Google Tag Manager.
    - This change prepares the application for a more efficient tracking setup with Cloudflare Turnstile.

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,20 +36,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         />
         {/* End Google Tag Manager */}
         
-        {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-1BY36XK59X"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-1BY36XK59X');
-            `
-          }}
-        />
-        {/* End Google tag (gtag.js) */}
-        
         {/* Cloudflare Turnstile */}
         <script
           src="https://challenges.cloudflare.com/turnstile/v0/api.js"
